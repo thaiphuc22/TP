a17fdfe6b649405dbdbe776a06e651327f63a0f5
thaiphuc22 <thaiphuc22@gmail.com>
2024-07-19 09:06:43 +0700

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+    $('.owl-carousel').owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: false,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      dots: false,
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 1
+        },
+        1000: {
+          items: 1
+        }
+      }
+    })
+  
+  
+  });
